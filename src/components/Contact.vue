b5d4f352377c5f6c8925c5dcdd87e2046c40e201
Tobeworks <hallo@tobeworks.de>
2024-04-19 12:58:04 +0200

    Contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,48 +1,79 @@
 <template>
     <section class="main-section bg-secondary-400" id="contact">
-        <div class="container">
-            <div id="msg_success" v-if="msg_success" class="alert alert-success" role="alert">
-                Your Message has been sent
-            </div>
-            <div id="msg_error" v-if="msg_error" class="alert alert-danger" role="alert">
-                Error sending your message
-            </div>
+        <div class="container mx-auto">
+
             <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
                 <div>
-                    <h2 class="text-right" id="headline-contact">Contact</h2>
+                    <h2 class="text-right text-2xl font-bold" id="headline-contact">Contact</h2>
                 </div>
                 <div>
                     <form id="contactform" name="contact-form" @submit.prevent="sendForm" action="#contact" method="post">
                         <div class="mb-3">
-                            <label>Email address</label>
-                            <input type="email" class="form-control" name="email" v-model="form_email" required :disabled="form_disabled" />
+                            <label class="block text-gray-700 text-sm font-bold mb-2">Email address</label>
+                            <input type="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="email" v-model="form_email" required :disabled="form_disabled" />
                         </div>
                         <div class="mb-3">
-                            <label>Name</label>
-                            <input type="text" class="form-control" name="name" v-model="form_name" required :disabled="form_disabled" />
+                            <label class="block text-gray-700 text-sm font-bold mb-2">Name</label>
+                            <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="name" v-model="form_name" required :disabled="form_disabled" />
                         </div>
                         <div class="mb-3">
-                            <label>Message</label>
-                            <textarea class="form-control textarea" rows="3" name="message" v-model="form_message" required :disabled="form_disabled"></textarea>
+                            <label class="block text-gray-700 text-sm font-bold mb-2">Message</label>
+                            <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" rows="3" name="message" v-model="form_message" required :disabled="form_disabled"></textarea>
                         </div>
-                        <button type="submit" class="btn btn-lm" :disabled="form_disabled">Send me a message</button>
+                        <button type="submit" class="bg-primary-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" :disabled="form_disabled">Send me a message</button>
                     </form>
-                    <div class="alert alert-light m-3" role="alert" id="mail-alert" style="display: none;">
+                    <div class="bg-gray-100 border border-gray-400 text-gray-700 px-4 py-3 rounded relative m-3" role="alert" id="mail-alert" style="display: none;">
+                    </div>
+
+                    <div id="msg_success" v-if="msg_success" class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative my-2" role="alert">
+                        Your Message has been sent
+                    </div>
+                    <div id="msg_error" v-if="msg_error" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative my-2" role="alert">
+                        Error sending your message
                     </div>
                 </div>
             </div>
+
         </div>
     </section>
 </template>
 
 <script setup>
 import axios from 'axios'
+import { ref } from 'vue'
 
 const form_email = ref(null);
 const form_disabled = ref(false);
 const form_name = ref(null);
 const form_message = ref(null);
 const uid = 'AzzoyqsYxvPH338XZRvm3Wgar';
+const msg_error = ref(false);
+const msg_success = ref(false);
 
+function sendForm() {
 
+    form_disabled.value = true;
+
+    axios.post('https://api.tobeworks.de/sendmail', {
+        name: form_name.value,
+        email: form_email.value,
+        message: form_message.value,
+        uid: uid
+    })
+        .then(response => {
+            if (response.data.status == '1') {
+                msg_success.value = true;
+            }
+            if (response.data.status == '0') {
+                msg_error.value = true;
+            }
+            form_disabled.value = false;
+            // console.log(response.data.status);
+        })
+        // eslint-disable-next-line no-unused-vars
+        .catch(function (error) {
+            //console.log(error);
+            msg_error.value = true;
+        });
+}
 </script>
