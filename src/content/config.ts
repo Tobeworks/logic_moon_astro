8ff7269fd6e2ac06fde55c0d0a0f9b47036f018a
Tobias Lorsbach <tlorsbach@m-m-m.de>
2025-07-02 15:40:47 +0200

    feat: blog setup

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,6 +11,18 @@ const cover_art = defineCollection({
     })
 });
 
+const blog = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.date(),
+        image: z.string().optional(),
+        draft: z.boolean().default(false),
+    }),
+});
+
 export const collections = {
     cover_art,
+    blog,
 };
