b36f6d451f44915d5b38c17100e033f0a73d03f2
Tobias Lorsbach <hallo@tobeworks.de>
2026-05-27 12:24:20 +0200

    fix: modal scroll lock

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -41,22 +41,12 @@ const props = defineProps({
 
 const emit = defineEmits(['close']);
 
-let scrollY = 0;
-
 const lockScroll = () => {
-    scrollY = window.scrollY;
-    document.body.style.position = 'fixed';
-    document.body.style.top = `-${scrollY}px`;
-    document.body.style.width = '100%';
-    document.body.style.overflowY = 'scroll';
+    window.lenis?.stop();
 };
 
 const unlockScroll = () => {
-    document.body.style.position = '';
-    document.body.style.top = '';
-    document.body.style.width = '';
-    document.body.style.overflowY = '';
-    window.scrollTo(0, scrollY);
+    window.lenis?.start();
 };
 
 const handleClose = () => {
